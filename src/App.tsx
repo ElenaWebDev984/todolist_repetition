cc9d0904a1e1971e64794fa644828a9d055d2065
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-01 21:15:40 +0200

    created tasksArrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import {TodolistItem} from "./TodolistItem.tsx";
+import type {Tasks} from "./TasksList.tsx";
 
 
 export const App = () => {
+    const tasks1: Tasks[] = [
+        { id: 1, title: 'HTML&CSS', isDone: true },
+        { id: 2, title: 'JS', isDone: true },
+        { id: 3, title: 'ReactJS', isDone: false },
+    ]
+
+    const tasks2: Tasks[] = [
+        { id: 1, title: 'Milk', isDone: true },
+        { id: 2, title: 'Orange', isDone: false },
+        { id: 3, title: 'Apple', isDone: false },
+    ]
+
     return (
         <div className="App">
-            <TodolistItem title='What to learn'/>
-            <TodolistItem title='What to buy'/>
-            <TodolistItem title='What to read'/>
+            <TodolistItem title='What to learn' tasks={tasks1}/>
+            <TodolistItem title='What to buy' tasks={tasks2}/>
         </div>
     );
 };
