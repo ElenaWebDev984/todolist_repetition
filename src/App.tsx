c6c4a971636fca7ccc24e147c570726b1263266d
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-01 19:43:48 +0200

    created TodolistItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
+import {TodolistItem} from "./TodolistItem.tsx";
 
 
 const App = () => {
     return (
-        <div>
-            <h3>What to learn</h3>
-            <div>
-                <input type="text"/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li>
-                    <input type="checkbox"checked={true}/>
-                    <span>HTML&CSS</span>
-                </li>
-                <li>
-                    <input type="checkbox"checked={true}/>
-                    <span>JS</span>
-                </li>
-                <li>
-                    <input type="checkbox"checked={false}/>
-                    <span>React</span>
-                </li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>   
-
-        </div>
+       <TodolistItem/>
     );
 };
 
---
+++ src/TodolistItem.tsx
@@ -0,0 +1,34 @@
+
+
+export const TodolistItem = () => {
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input type="text"/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox" checked={true}/>
+                    <span>HTML&CSS</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={true}/>
+                    <span>JS</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={false}/>
+                    <span>React</span>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+
+        </div>
+    );
+};
+
