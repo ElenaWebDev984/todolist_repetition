3664168ae9c333931778e42e32790a2c8d22a3b6
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-06 01:07:16 +0200

    added changeFilter

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -2,15 +2,17 @@ import {TodolistTitle} from "./TodolistTitle.tsx";
 import {AddTaskForm} from "./AddTaskForm.tsx";
 import {Button} from "./Button.tsx";
 import {type Tasks, TasksList} from "./TasksList.tsx";
+import type {FilterValues} from "./App.tsx";
 
 type TodolistItemTypes = {
     title: string
     tasks: Tasks[]
     deleteTask: (id: number) => void
+    changeFilter: (filter: FilterValues) => void
 }
 
 
-export const TodolistItem = ({title, tasks, deleteTask}: TodolistItemTypes) => {
+export const TodolistItem = ({title, tasks, deleteTask, changeFilter}: TodolistItemTypes) => {
     return (
         <div className='container'>
             <TodolistTitle title={title}/>
@@ -18,9 +20,9 @@ export const TodolistItem = ({title, tasks, deleteTask}: TodolistItemTypes) => {
             <TasksList tasks={tasks}
                        deleteTask={deleteTask}/>
             <div className='btn-wrapper'>
-                <Button title='All'/>
-                <Button title='Active'/>
-                <Button title='completed'/>
+                <Button title='All' onClick={() => changeFilter('all')}/>
+                <Button title='Active' onClick={() => changeFilter('active')}/>
+                <Button title='completed' onClick={() => changeFilter('completed')}/>
             </div>
 
         </div>
